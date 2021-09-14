02ab24b39a6b730e48da4422b99d3b3d6bf878ae
LeoAndo <leo.777.info@gmail.com>
2021-09-15 00:07:57 +0900

    Vector2の仕様

--- 2DScriptSample/Assets/Test.cs
+++ 2DScriptSample/Assets/Test.cs
@@ -1,18 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; // Unityを動かすために必要な機能を提供する
 
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Start!");
+        Vector2 objPosition = new Vector2(5, 4);
+        objPosition.x += 6;
+        objPosition.y += 8;
+        Debug.Log(objPosition);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Debug.Log("Update!");
     }
+
+
 }
