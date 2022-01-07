80fd61f665a7c4f9d2b7051c94848a770fb02578
LeoAndo <leo.777.info@gmail.com>
2022-01-07 19:12:27 +0900

    ボタンを押したときにプレイヤを移動させる

--- CatEscape/Assets/PlayerController.cs
+++ CatEscape/Assets/PlayerController.cs
@@ -10,18 +10,30 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
+    // LButtonDown
+    public void LButtonDown()
     {
-        // 左矢印が押された時
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            transform.Translate(-3, 0, 0); // 左に「3」動かす
-        }
-        // 右矢印が押された時
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            transform.Translate(3, 0, 0); // 右に「3」動かす
-        }
+        transform.Translate(-3, 0, 0); // 左に「3」動かす
     }
+    public void RButtonDown()
+    {
+        transform.Translate(3, 0, 0); // 右に「3」動かす
+    }
+
+    /*
+        // Update is called once per frame
+        void Update()
+        {
+            // 左矢印が押された時
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+
+            }
+            // 右矢印が押された時
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                transform.Translate(3, 0, 0); // 右に「3」動かす
+            }
+        }
+        */
 }
