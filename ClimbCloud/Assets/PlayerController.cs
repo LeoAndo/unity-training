0a83227261774314c9890fabb0669d0eebf27d41
LeoAndo <leo.777.info@gmail.com>
2022-01-13 15:19:57 +0900

    矢印キーに応じて移動する

--- ClimbCloud/Assets/PlayerController.cs
+++ ClimbCloud/Assets/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
     float jumpForce = 680.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +21,26 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector2.up * jumpForce);
+        }
+
+        // move left/right
+        int key = 0;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            key = -1;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            key = 1;
+        }
+
+        // speed
+        float speedx = Mathf.Abs(rb.velocity.x);
+
+        // speed limit
+        if (speedx < maxWalkSpeed)
+        {
+            rb.AddForce(Vector2.right * key * walkForce);
         }
     }
 }
