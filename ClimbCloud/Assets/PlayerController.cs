11c3cc667807730cb073837a46a466152b43e7be
LeoAndo <leo.777.info@gmail.com>
2022-01-14 15:15:08 +0900

    シーンの登録

--- ClimbCloud/Assets/PlayerController.cs
+++ ClimbCloud/Assets/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -57,5 +58,6 @@ public class PlayerController : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("GOAL!");
+        SceneManager.LoadScene("ClearScene");
     }
 }
