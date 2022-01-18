c8538c0c5979ed28cf4e91313a03e9e856f23995
LeoAndo <leo.777.info@gmail.com>
2022-01-18 13:30:55 +0900

    オブジェクトを飛ばす(3Dへん)

--- Igaguri/Assets/IgaguriGenerator.cs
+++ Igaguri/Assets/IgaguriGenerator.cs
@@ -16,7 +16,12 @@ public class IgaguriGenerator : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             GameObject igaguri = Instantiate(igaguriPrefab);
-            igaguri.GetComponent<IgaguriController>().Shoot(new Vector3(0, 200, 2000));
+            // 決まった位置に いがぐり を飛ばす
+            // igaguri.GetComponent<IgaguriController>().Shoot(new Vector3(0, 200, 2000));
+            // タップした場所に いがぐり を飛ばす
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            igaguri.GetComponent<IgaguriController>().Shoot(worldDir.normalized * 2000);
         }
     }
 }
