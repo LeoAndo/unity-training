eacd9ccb0f9f52d2348197539f1abe92cc0cde40
LeoAndo <leo.777.info@gmail.com>
2022-01-18 13:00:07 +0900

    add prefab

--- Igaguri/Assets/IgaguriController.cs
+++ Igaguri/Assets/IgaguriController.cs
@@ -7,7 +7,7 @@ public class IgaguriController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Shoot(new Vector3(0, 200, 2000));
+        // Shoot(new Vector3(0, 200, 2000));
     }
 
     // Update is called once per frame
