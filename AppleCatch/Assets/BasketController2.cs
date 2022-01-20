f3eca827309a7f5d5abbec6d90e4c38839812e52
LeoAndo <leo.777.info@gmail.com>
2022-01-20 19:34:31 +0900

    basketを移動するコントローラスクリプトの作成

---
+++ AppleCatch/Assets/BasketController2.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketController2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // タップされたスクリーン座標(Input.mousePosition)を元にして、バスケットを移動させる座標を計算する.
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //スクリーン座標をワールド座標に変換
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                float x = Mathf.RoundToInt(hit.point.x);
+                float z = Mathf.RoundToInt(hit.point.z);
+                transform.position = new Vector3(x, 0, z);
+            }
+        }
+    }
+}
